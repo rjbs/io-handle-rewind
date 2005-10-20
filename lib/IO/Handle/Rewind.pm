bbb5a64490ad963f482db0b29bed9fa492722707
hdp <hdp@1bcdbe44-fcfd-0310-b51b-975661d93aa0>
2005-10-20 14:09:59 +0000

    r15846@vex:  hdp | 2005-10-20 10:10:26 -0400  0.04: make META.yml and Rewind.pm agree on version, list Class::Accessor as a dependency
    git-svn-id: svn+ssh://svn.pobox.com/svn/io-handle-rewind@15337 1bcdbe44-fcfd-0310-b51b-975661d93aa0

--- lib/IO/Handle/Rewind.pm
+++ lib/IO/Handle/Rewind.pm
@@ -4,7 +4,7 @@ IO::Handle::Rewind - pretend to rewind filehandles
 
 =head1 VERSION
 
-0.03
+0.04
 
 =head1 DESCRIPTION
 
@@ -58,7 +58,7 @@ use strict;
 use Carp qw(croak);
 use base qw(Class::Accessor);
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 __PACKAGE__->mk_accessors(qw(rewound obj));
 
